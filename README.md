df73c5ab3da850e19855f6909528b54e63f4fb07
Adam Oliver <mail@adamoliver.net>
2017-01-13 15:56:06 +0000

    Add plugin

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # videojs-splash-play
+
+Replaces the default videojs play icon with a custom icon for video and audio players
+
+## Installation
+
+```sh
+npm install --save videojs-splash-play
+```
+
+The npm installation is preferred, but Bower works, too.
+
+```sh
+bower install  --save videojs-splash-play
+```
+
+## Usage
+
+To include videojs-splash-play on your website or web application, use any of the following methods.
+
+### `<script>` Tag
+
+This is the simplest case. Get the script in whatever way you prefer and include the plugin _after_ you include [video.js][videojs], so that the `videojs` global is available.
+
+```html
+<script src="//path/to/video.min.js"></script>
+<script src="//path/to/videojs-splash-play.min.js"></script>
+<script>
+  var player = videojs('my-video');
+
+  player.splashPlay();
+</script>
+```
+
+### Browserify
+
+When using with Browserify, install videojs-splash-play via npm and `require` the plugin as you would any other module.
+
+```js
+var videojs = require('video.js');
+
+// The actual plugin function is exported by this module, but it is also
+// attached to the `Player.prototype`; so, there is no need to assign it
+// to a variable.
+require('videojs-splash-play');
+
+var player = videojs('my-video');
+
+player.splashPlay();
+```
+
+### RequireJS/AMD
+
+When using with RequireJS (or another AMD library), get the script in whatever way you prefer and `require` the plugin as you normally would:
+
+```js
+require(['video.js', 'videojs-splash-play'], function(videojs) {
+  var player = videojs('my-video');
+
+  player.splashPlay();
+});
+```
+
+## License
+
+MIT. Copyright (c) Adam Oliver &lt;mail@adamoliver.net&gt;
+
+
+[videojs]: http://videojs.com/
---
+++ src/plugin.js
@@ -0,0 +1,78 @@
+import videojs from 'video.js';
+
+// Default options for the plugin.
+const defaults = {
+    cssClassPrefix: 'icon',
+    largePlayerSize: 500
+};
+
+/**
+ * A video.js plugin.
+ *
+ * In the plugin function, the value of `this` is a video.js `Player`
+ * instance. You cannot rely on the player being in a "ready" state here,
+ * depending on how the plugin is invoked. This may or may not be important
+ * to you; if not, remove the wait for "ready"!
+ *
+ * @function splashPlay
+ * @param    {Object} [options={}]
+ *           An object of options left to the plugin author to define.
+ */
+const splashPlay = function(options) {
+  this.ready(() => {
+    let options = videojs.mergeOptions(defaults, options);
+
+    let player = this;
+    let splashButton = document.createElement("div");
+
+    // Create the CSS class name for the button
+    splashButton.className = "vjs-splash-play";
+    let buttonClassName = player.isAudio() ?
+        `${options.cssClassPrefix} ${options.cssClassPrefix}-audio` :
+        `${options.cssClassPrefix} ${options.cssClassPrefix}-play`;
+    splashButton.className += ` ${buttonClassName}`;
+
+    // Play on click
+    splashButton.addEventListener('click', play);
+
+    // Add the button to the player
+    player.el().appendChild(splashButton);
+
+    // When the player is playing, hide the button
+    player.on('play', hideButton);
+
+    resize();
+
+    window.addEventListener('resize', resize, true);
+
+    function play () {
+        player.play();
+        hideButton();
+    }
+
+    function hideButton () {
+        splashButton.style.display = 'none'
+    }
+
+    function resize () {
+        // Add an additional class to the button if the player is larger
+        let button = document.querySelectorAll(`#${player.id()} .vjs-splash-play`);
+        let largeCssClass = 'vjs-splash-play-large';
+
+        if (player.el().offsetWidth > options.largePlayerSize) {
+            button[0].classList.add(largeCssClass);
+        } else {
+            button[0].classList.remove(largeCssClass);
+        }
+    }
+
+  });
+};
+
+// Register the plugin with video.js.
+videojs.plugin('splashPlay', splashPlay);
+
+// Include the version number.
+splashPlay.VERSION = '__VERSION__';
+
+export default splashPlay;
